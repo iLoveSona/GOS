fa1561c7979987b5576d2cdd8bc2f72c00ba3be6
iLoveSona <iLoveSona@gos.com>
2015-09-06 02:34:11 +0800

    [simple auto level spell] add lv4

--- simple auto level spell.lua
+++ simple auto level spell.lua
@@ -1,19 +1,20 @@
 require ("DLib")
 
 local spellTable = {_Q, _W, _E, _R}
--- local spellTablePre3 = {_Q, _W, _E}
+-- local spellTablePre4 = {_Q, _W, _E}
 
 local name = GetObjectName(GetMyHero())
 
 local submenu = menu.addItem(SubMenu.new("simple auto level spell for "..name))
 local disableAtLv1 = submenu.addItem(MenuBool.new("disable auto level spell at Lv1", true))
-local submenuSpellTablePre3 = submenu.addItem(SubMenu.new("level spell sequence before Lv3"))
-local submenuSpellTablePre3List = {
-	submenuSpellTablePre3.addItem(MenuStringList.new("Lv1", {"Q", "W", "E", "R"}, 1)),
-	submenuSpellTablePre3.addItem(MenuStringList.new("Lv2", {"Q", "W", "E", "R"}, 2)),
-	submenuSpellTablePre3.addItem(MenuStringList.new("Lv3", {"Q", "W", "E", "R"}, 3))
+local submenuSpellTablePre4 = submenu.addItem(SubMenu.new("level spell sequence before Lv4"))
+local submenuSpellTablePre4List = {
+	submenuSpellTablePre4.addItem(MenuStringList.new("Lv1", {"Q", "W", "E", "R"}, 1)),
+	submenuSpellTablePre4.addItem(MenuStringList.new("Lv2", {"Q", "W", "E", "R"}, 2)),
+	submenuSpellTablePre4.addItem(MenuStringList.new("Lv3", {"Q", "W", "E", "R"}, 3)),
+	submenuSpellTablePre4.addItem(MenuStringList.new("Lv4", {"Q", "W", "E", "R"}, 1)),
 }
-local submenuSpellTable = submenu.addItem(SubMenu.new("level spell sequence after Lv3"))
+local submenuSpellTable = submenu.addItem(SubMenu.new("level spell sequence after Lv4"))
 local submenuSpellTableList = {
 	submenuSpellTable.addItem(MenuStringList.new("first", {"Q", "W", "E", "R"}, 4)),
  	submenuSpellTable.addItem(MenuStringList.new("second", {"Q", "W", "E", "R"}, 1)),
@@ -28,10 +29,11 @@ local spellSequence = {
 	spellTable[submenuSpellTableList[3].getValue()],
 	spellTable[submenuSpellTableList[4].getValue()],
 }
-local spellSequencePre3 = {
-	spellTable[submenuSpellTablePre3List[1].getValue()],
-	spellTable[submenuSpellTablePre3List[2].getValue()],
-	spellTable[submenuSpellTablePre3List[3].getValue()],
+local spellSequencePre4 = {
+	spellTable[submenuSpellTablePre4List[1].getValue()],
+	spellTable[submenuSpellTablePre4List[2].getValue()],
+	spellTable[submenuSpellTablePre4List[3].getValue()],
+	spellTable[submenuSpellTablePre4List[4].getValue()],
 }
 
 -- local function init()
@@ -41,10 +43,10 @@ local spellSequencePre3 = {
 -- 		spellTable[submenuSpellTableList[3].getValue()],
 -- 		spellTable[submenuSpellTableList[4].getValue()],
 -- 	}
--- 	spellSequencePre3 = {
--- 		spellTable[submenuSpellTablePre3List[1].getValue()],
--- 		spellTable[submenuSpellTablePre3List[2].getValue()],
--- 		spellTable[submenuSpellTablePre3List[3].getValue()],
+-- 	spellSequencePre4 = {
+-- 		spellTable[submenuSpellTablePre4List[1].getValue()],
+-- 		spellTable[submenuSpellTablePre4List[2].getValue()],
+-- 		spellTable[submenuSpellTablePre4List[3].getValue()],
 -- 	}
 -- end
 
@@ -53,8 +55,8 @@ local spellSequencePre3 = {
 -- PrintChat(spellSequence[1]..spellSequence[2]..spellSequence[3]..spellSequence[4])
 local lv = 0
 
-local function doLevelSpellPre3()
-	LevelSpell(spellSequencePre3[lv])
+local function doLevelSpellPre4()
+	LevelSpell(spellSequencePre4[lv])
 end
 
 local function doLevelSpell()
@@ -72,8 +74,8 @@ OnLoop(function(myHero)
 
 	if disableAtLv1.getValue() and lv == 1 then return end
 	-- if lv < 6 then return end
-	if lv <= 3 and spellSequencePre3 then
-		doLevelSpellPre3()
+	if spellSequencePre4 and lv <= #spellSequencePre4 then
+		doLevelSpellPre4()
 	else
 		doLevelSpell()
 	end
@@ -84,10 +86,10 @@ end)
 -- api table
 local s = {}
 
- function s.setLvSpellSequence(m_spellSequence, m_spellSequencePre3, m_disableAtLv1)
+ function s.setLvSpellSequence(m_spellSequence, m_spellSequencePre4, m_disableAtLv1)
 	-- TODO should change the menu value instead just change list directly
 	spellSequence = m_spellSequence
-	spellSequencePre3 = m_spellSequencePre3
+	spellSequencePre4 = m_spellSequencePre4
 	disableAtLv1.setValue(m_disableAtLv1 or false)
 	-- init()
 
