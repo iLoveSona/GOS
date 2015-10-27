449212a7c56ed129678004baa461435dc6c44ad5
iLoveSona <iLoveSona@gos.com>
2015-10-27 12:45:33 +0800

    vayne with better stun logic

--- simple vayne.lua
+++ simple vayne.lua
@@ -7,19 +7,22 @@ d = require 'DLib'
 local IsInDistance = d.IsInDistance
 local ValidTarget = d.ValidTarget
 local GetDistance = d.GetDistance
+local GetTarget = d.GetTarget
 
 local submenu = menu.addItem(SubMenu.new("simple vayne"))
 local key = submenu.addItem(MenuKeyBind.new("auto stun key", string.byte(" ")))
 local autoStun = submenu.addItem(MenuBool.new("auto stun when possible", true))
 local combo = submenu.addItem(MenuKeyBind.new("combo key", string.byte(" ")))
 
+local eRange = GetCastRange(myHero,_E)
+
 -- modify from IAC vayne
-local function AutoEiAC()
-	local target = GetCurrentTarget()
-	if ValidTarget(target,GetCastRange(myHero,_E)) and IsInDistance(target, GetCastRange(myHero,_E)) and CanUseSpell(myHero,_E) == READY then
-		for _=0,450,GetHitBox(target) do
-			local Pred = GetPredictionForPlayer(GetOrigin(myHero),target,GetMoveSpeed(target), 2000, 250, 1000, 1, false, true)
-	    local tPos = Vector(Pred.PredPos)+Vector(Vector(Pred.PredPos)-Vector(myHero)):normalized()*_
+local function AutoEiAC(target)
+	if ValidTarget(target,eRange) and CanUseSpell(myHero,_E) == READY then
+		local pos = GetPredictionForPlayer(GetOrigin(myHero),target,GetMoveSpeed(target), 2000, 250, 1000, 1, false, true).PredPos
+		local vPos = Vector(pos)
+		for length = 0, 450, GetHitBox(target) do
+	    local tPos = vPos + Vector(vPos - Vector(myHero)):normalized() * length
 	    if MapPosition:inWall(tPos) then
 	      CastTargetSpell(target, _E)
 	      break
@@ -39,7 +42,7 @@ end)
 
 OnTick(function(myHero)
 	if key.getValue() or autoStun.getValue() then
-		AutoEiAC()
+		AutoEiAC(GetTarget(eRange, DAMAGE_PHYSICAL))
 	end
 
 	-- if shouldReset then
